09da0f7e79280632124bbff6fd00f3409a7ca8af
Gayathri <gayathri_buddappagari@cable.comcast.com>
2017-03-06 18:36:47 +0530

    Added more tests for nopoll_helper.c

--- tests/test_nopoll_helpers.c
+++ tests/test_nopoll_helpers.c
@@ -13,95 +13,255 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+ 
 #include <stdarg.h>
-
-#include <CUnit/Basic.h>
-#include <stdbool.h>
-
-#include <assert.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
 #include <nopoll.h>
 
-//#include <nanomsg/bus.h>
-
-#include "../src/ParodusInternal.h"
+#include "../src/parodus_log.h"
 #include "../src/nopoll_helpers.h"
-#include "wrp-c.h"
 
-#include<errno.h>
-
-/* Nanomsg related Macros */
-#define ENDPOINT "tcp://127.0.0.1:6666"
-#define CLIENT1_URL "tcp://127.0.0.1:6667"
-#define CLIENT2_URL "tcp://127.0.0.1:6668"
-#define CLIENT3_URL "tcp://127.0.0.1:6669"
-
+/*----------------------------------------------------------------------------*/
+/*                                   Macros                                   */
+/*----------------------------------------------------------------------------*/
+#define UNUSED(x) (void )(x)
+#define MAX_SEND_SIZE (60 * 1024)
+/*----------------------------------------------------------------------------*/
+/*                            File Scoped Variables                           */
+/*----------------------------------------------------------------------------*/
+ static noPollConn *conn;
 /*----------------------------------------------------------------------------*/
 /*                                   Mocks                                    */
 /*----------------------------------------------------------------------------*/
-nopoll_bool nopoll_conn_is_ok__rv = nopoll_false;
 nopoll_bool nopoll_conn_is_ok( noPollConn *conn )
 {
-	UNUSED(conn);
-    return nopoll_conn_is_ok__rv;
+    ParodusInfo("function_called : %s\n",__FUNCTION__);
+    function_called();
+	check_expected(conn);
+	
+    return (nopoll_bool)mock();
 }
 
-nopoll_bool nopoll_conn_is_ready__rv = nopoll_false;
 nopoll_bool nopoll_conn_is_ready( noPollConn *conn )
 {
-	UNUSED(conn);
-    return nopoll_conn_is_ready__rv;
+    ParodusInfo("function_called : %s\n",__FUNCTION__);
+    function_called();
+	check_expected(conn);
+	
+    return (nopoll_bool)mock();
 }
 
-int nopoll_conn_send_binary_rv = 0;
-int nopoll_conn_send_binary( noPollConn *conn, const char *content, long length ) 	
+int  __nopoll_conn_send_common (noPollConn * conn, const char * content, long length, nopoll_bool  has_fin, long       sleep_in_header, noPollOpCode frame_type)
 {
-	UNUSED(conn);
-	UNUSED(content);
-	UNUSED(length);
-    return nopoll_conn_send_binary_rv;
+    UNUSED(has_fin); UNUSED(sleep_in_header); UNUSED(frame_type); UNUSED(content);
+    ParodusInfo("function_called : %s\n",__FUNCTION__);
+    function_called();
+    
+    check_expected(conn);
+    check_expected(length);
+    return (int)mock();
+}
+
+int nopoll_conn_flush_writes(noPollConn * conn, long timeout, int previous_result)
+{
+    UNUSED(timeout);
+    
+    ParodusInfo("function_called : %s\n",__FUNCTION__);
+    function_called();
+    
+    check_expected(conn);
+    check_expected(previous_result);
+    return (int)mock();
+}
+
+noPollConn *get_global_conn()
+{
+     return conn;   
+}
+
+void listenerOnMessage_queue(noPollCtx * ctx, noPollConn * conn, noPollMsg * msg,noPollPtr user_data)
+{
+    UNUSED(ctx); UNUSED(conn); UNUSED(msg); UNUSED(user_data);
+}
+
+void listenerOnPingMessage (noPollCtx * ctx, noPollConn * conn, noPollMsg * msg, noPollPtr user_data)
+{
+    UNUSED(ctx); UNUSED(conn); UNUSED(msg); UNUSED(user_data);
+}
+
+void listenerOnCloseMessage (noPollCtx * ctx, noPollConn * conn, noPollPtr user_data)
+{
+    UNUSED(ctx); UNUSED(conn); UNUSED(user_data);
 }
 
 /*----------------------------------------------------------------------------*/
 /*                                   Tests                                    */
 /*----------------------------------------------------------------------------*/
+void test_setMessageHandlers()
+{
+    setMessageHandlers();
+}
+
+void test_sendResponse()
+{
+    int len = strlen("Hello Parodus!");
+    expect_value(__nopoll_conn_send_common, conn, conn);
+    expect_value(__nopoll_conn_send_common, length, len);
+    will_return(__nopoll_conn_send_common, len);
+    expect_function_calls(__nopoll_conn_send_common, 1);
+    
+    int bytesWritten = sendResponse(conn, "Hello Parodus!", len);
+    ParodusPrint("Number of bytes written: %d\n", bytesWritten);
+    assert_int_equal(bytesWritten, len);
+}
+
+void test_sendResponseWithFragments()
+{
+    int len = (MAX_SEND_SIZE*2)+64;
+    
+    expect_value(__nopoll_conn_send_common, conn, conn);
+    expect_value(__nopoll_conn_send_common, length, MAX_SEND_SIZE);
+    will_return(__nopoll_conn_send_common, MAX_SEND_SIZE);
+    expect_value(__nopoll_conn_send_common, conn, conn);
+    expect_value(__nopoll_conn_send_common, length, MAX_SEND_SIZE);
+    will_return(__nopoll_conn_send_common, MAX_SEND_SIZE);
+    expect_value(__nopoll_conn_send_common, conn, conn);
+    expect_value(__nopoll_conn_send_common, length, 64);
+    will_return(__nopoll_conn_send_common, 64);
+    expect_function_calls(__nopoll_conn_send_common, 3);
+    int bytesWritten = sendResponse(conn, "Hello Parodus!", len);
+    ParodusPrint("Number of bytes written: %d\n", bytesWritten);
+    assert_int_equal(bytesWritten, len);
+}
+
+void err_sendResponse()
+{
+    int len = strlen("Hello Parodus!");
+    
+    expect_value(__nopoll_conn_send_common, conn, conn);
+    expect_value(__nopoll_conn_send_common, length, len);
+    will_return(__nopoll_conn_send_common, -1);
+    expect_function_calls(__nopoll_conn_send_common, 1);
+    
+    int bytesWritten = sendResponse(conn, "Hello Parodus!", len);
+    ParodusPrint("Number of bytes written: %d\n", bytesWritten);
+    assert_int_equal(bytesWritten, 0);   
+}
+
+void err_sendResponseFlushWrites()
+{
+    int len = strlen("Hello Parodus!");
+    
+    expect_value(__nopoll_conn_send_common, conn, conn);
+    expect_value(__nopoll_conn_send_common, length, len);
+    will_return(__nopoll_conn_send_common, len-3);
+    expect_function_calls(__nopoll_conn_send_common, 1);
+    
+    expect_value(nopoll_conn_flush_writes, conn, conn);
+    expect_value(nopoll_conn_flush_writes, previous_result, len-3);
+    will_return(nopoll_conn_flush_writes, len-3);
+    expect_function_calls(nopoll_conn_flush_writes, 1);
+    
+    int bytesWritten = sendResponse(conn, "Hello Parodus!", len);
+    ParodusPrint("Number of bytes written: %d\n", bytesWritten);
+    assert_int_equal(bytesWritten, 0);   
+}
+
+void err_sendResponseConnNull()
+{
+    int len = strlen("Hello Parodus!");
+    expect_value(__nopoll_conn_send_common, conn, NULL);
+    expect_value(__nopoll_conn_send_common, length, len);
+    will_return(__nopoll_conn_send_common, -1);
+    expect_function_calls(__nopoll_conn_send_common, 1);
+    
+    int bytesWritten = sendResponse(NULL, "Hello Parodus!", len);
+    ParodusPrint("Number of bytes written: %d\n", bytesWritten);
+    assert_int_equal(bytesWritten, 0);   
+}
+
 void test_sendMessage()
 {
-    sendMessage(NULL, "hello", 6);
+    int len = strlen("Hello Parodus!");
+    
+    expect_value(nopoll_conn_is_ok, conn, conn);
+    will_return(nopoll_conn_is_ok, nopoll_true);
+    expect_function_call(nopoll_conn_is_ok);
+    
+    expect_value(nopoll_conn_is_ready, conn, conn);
+    will_return(nopoll_conn_is_ready, nopoll_true);
+    expect_function_call(nopoll_conn_is_ready);
+    
+    expect_value(__nopoll_conn_send_common, conn, conn);
+    expect_value(__nopoll_conn_send_common, length, len);
+    will_return(__nopoll_conn_send_common, len);
+    expect_function_calls(__nopoll_conn_send_common, 1);
+    
+    sendMessage(conn, "Hello Parodus!", len);
 }
 
-void add_suites( CU_pSuite *suite )
+void err_sendMessage()
 {
-    ParodusInfo("--------Start of Test Cases Execution ---------\n");
-    *suite = CU_add_suite( "tests", NULL, NULL );
-    CU_add_test( *suite, "Test sendMessage()", test_sendMessage );
+    int len = strlen("Hello Parodus!");
+    
+    expect_value(nopoll_conn_is_ok, conn, conn);
+    will_return(nopoll_conn_is_ok, nopoll_true);
+    expect_function_call(nopoll_conn_is_ok);
+    
+    expect_value(nopoll_conn_is_ready, conn, conn);
+    will_return(nopoll_conn_is_ready, nopoll_true);
+    expect_function_call(nopoll_conn_is_ready);
+    
+    expect_value(__nopoll_conn_send_common, conn, conn);
+    expect_value(__nopoll_conn_send_common, length, len);
+    will_return(__nopoll_conn_send_common, len-2);
+    expect_function_calls(__nopoll_conn_send_common, 1);
+    
+    expect_value(nopoll_conn_flush_writes, conn, conn);
+    expect_value(nopoll_conn_flush_writes, previous_result, len-2);
+    will_return(nopoll_conn_flush_writes, len-3);
+    expect_function_calls(nopoll_conn_flush_writes, 1);
+    
+    sendMessage(conn, "Hello Parodus!", len);
 }
 
+void err_sendMessageConnNull()
+{
+    int len = strlen("Hello Parodus!");
+    
+    expect_value(nopoll_conn_is_ok, conn, NULL);
+    will_return(nopoll_conn_is_ok, nopoll_false);
+    expect_function_call(nopoll_conn_is_ok);
+    
+    sendMessage(NULL, "Hello Parodus!", len);
+}
 
-
-
+void test_reportLog()
+{
+    __report_log(NULL, NOPOLL_LEVEL_DEBUG, "Debug", NULL);
+    __report_log(NULL, NOPOLL_LEVEL_INFO, "Info", NULL);
+    __report_log(NULL, NOPOLL_LEVEL_WARNING, "Warning", NULL);
+    __report_log(NULL, NOPOLL_LEVEL_CRITICAL, "Critical", NULL);
+}
 /*----------------------------------------------------------------------------*/
 /*                             External Functions                             */
 /*----------------------------------------------------------------------------*/
-int main( void )
+int main(void)
 {
-    unsigned rv = 1;
-    CU_pSuite suite = NULL;
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_setMessageHandlers),
+        cmocka_unit_test(test_sendResponse),
+        cmocka_unit_test(test_sendResponseWithFragments),
+        cmocka_unit_test(err_sendResponse),
+        cmocka_unit_test(err_sendResponseFlushWrites),
+        cmocka_unit_test(err_sendResponseConnNull),
+        cmocka_unit_test(test_sendMessage),
+        cmocka_unit_test(err_sendMessage),
+        cmocka_unit_test(err_sendMessageConnNull),
+        cmocka_unit_test(test_reportLog),
+    };
 
-    if( CUE_SUCCESS == CU_initialize_registry() ) {
-        add_suites( &suite );
-
-        if( NULL != suite ) {
-            CU_basic_set_mode( CU_BRM_VERBOSE );
-            CU_basic_run_tests();
-            ParodusPrint( "\n" );
-            CU_basic_show_failures( CU_get_failure_list() );
-            ParodusPrint( "\n\n" );
-            rv = CU_get_number_of_tests_failed();
-        }
-
-        CU_cleanup_registry();
-
-    }
-
-    return rv;
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
