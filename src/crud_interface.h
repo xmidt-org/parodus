b8c3111d6a8b4bb17b940862c05aedd6deb46392
Sadhyama Vengilat <Sadhyama_Vengilat2@cable.comcast.com>
2018-04-26 16:23:13 +0530

    Added queue mechanism to handle CRUD upsteam & downstream

---
+++ src/crud_interface.h
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2015 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * @file crud_interface.h
+ *
+ * @description This header defines functions required to manage CRUD messages.
+ *
+ */
+ 
+#ifndef _CRUD_INTERFACE_H_
+#define _CRUD_INTERFACE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*----------------------------------------------------------------------------*/
+/*                               Data Structures                              */
+/*----------------------------------------------------------------------------*/
+typedef struct CrudMsg__
+{
+	wrp_msg_t *msg;
+	struct CrudMsg__ *next;
+} CrudMsg;
+
+/*----------------------------------------------------------------------------*/
+/*                             Function Prototypes                            */
+/*----------------------------------------------------------------------------*/
+
+//void *CRUDHandlerTask();
+//void addCRUDmsgToQueue(wrp_msg_t *crudMsg);
+void addCRUDresponseToUpstreamQ(void *response_bytes, ssize_t response_size);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _CRUD_INTERFACE_H_ */
+
