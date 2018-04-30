2634856729fccdacdeec5c8b253ac0bab13784c6
mander207 <Minas_Anderiasian@cable.comcast.com>
2018-04-30 13:43:56 -0700

    Some cleanup, adding a MAC optional command line parameter

--- tests/producer.c
+++ tests/producer.c
@@ -30,9 +30,6 @@
 /*----------------------------------------------------------------------------*/
 /*                                   Macros                                   */
 /*----------------------------------------------------------------------------*/
-#define debug_error(...)      cimplog_error("printer", __VA_ARGS__)
-#define debug_info(...)       cimplog_info("printer", __VA_ARGS__)
-#define debug_print(...)      cimplog_debug("printer", __VA_ARGS__)
 
 
 #define SERVICE_NAME "printer"
@@ -41,14 +38,16 @@ static void sig_handler(int sig);
 static int main_loop(libpd_cfg_t *cfg);
 static void _help(char *, char *);
 
+static char *mac_address;
 
 int main( int argc, char **argv)
 {
-    const char *option_string = "p:c:w:d:f:m:h::";
+    const char *option_string = "p:c:w:d:f:m::h::";
     static const struct option options[] = {
         { "help",         optional_argument, 0, 'h' },
         { "parodus-url",  required_argument, 0, 'p' },
         { "client-url",   required_argument, 0, 'c' },
+	{ "mac-address",  optional_argument, 0, 'm'},
         { 0, 0, 0, 0 }
     };
 
@@ -76,6 +75,8 @@ int main( int argc, char **argv)
     signal(SIGHUP, sig_handler);
     signal(SIGALRM, sig_handler);
     
+    mac_address = NULL;
+
     while( -1 != (item = getopt_long(argc, argv, option_string, options, &opt_index)) ) {
         switch( item ) {
             case 'p':
@@ -87,6 +88,9 @@ int main( int argc, char **argv)
             case 'h':
                 _help(argv[0], optarg);
                 break;
+	    case 'm':
+		mac_address = strdup(optarg);
+		break;
             case '?':
                 if (strchr(option_string, optopt)) {
                     printf("%s Option %c requires an argument!\n", argv[0], optopt);
@@ -104,58 +108,62 @@ int main( int argc, char **argv)
         }
     }
 
+
     if( (rv == 0) &&
         (NULL != cfg.parodus_url) &&
         (NULL != cfg.client_url))
     {        
-        main_loop(&cfg);
+        if (NULL == mac_address) {
+            mac_address = strdup("14cfe1234567");
+	}
+	main_loop(&cfg);
         rv = 0;
     } else {
         if ((NULL == cfg.parodus_url)) {
-            debug_error("%s parodus_url not specified!\n", argv[0]);
+            printf("%s parodus_url not specified!\n", argv[0]);
             rv = -1;
         }
         if ((NULL == cfg.client_url)) {
-            debug_error("%s client_url not specified !\n", argv[0]);
+            printf("%s client_url not specified !\n", argv[0]);
             rv = -2;
         }        
      }
     
     if (rv != 0) {
-        debug_error("%s  program terminating\n", argv[0]);
+        printf("%s  program terminating\n", argv[0]);
     }
 
     if( NULL != cfg.parodus_url )   free( (char*) cfg.parodus_url );
     if( NULL != cfg.client_url )    free( (char*) cfg.client_url );
-
+    if( NULL != mac_address ) free((char *) mac_address);
     return rv;
 }
 
 static void sig_handler(int sig)
 {
     if( sig == SIGINT ) {
-        debug_info("SIGINT received! Program Terminating!\n");
+        printf("SIGINT received! Program Terminating!\n");
         exit(0);
     } else if ( sig == SIGTERM ) {
-        debug_info("SIGTERM received! Program Terminating!\n");
+        printf("SIGTERM received! Program Terminating!\n");
         exit(0);
     } else if( sig == SIGUSR1 ) {
         signal(SIGUSR1, sig_handler); /* reset it to this function */
-        debug_info("SIGUSR1 received!\n");
+        printf("SIGUSR1 received!\n");
     } else if( sig == SIGUSR2 ) {
         signal(SIGUSR2, sig_handler);
-        debug_info("SIGUSR2 received!\n");
+        printf("SIGUSR2 received!\n");
     } else if( sig == SIGCHLD ) {
         signal(SIGCHLD, sig_handler); /* reset it to this function */
-        debug_info("SIGHLD received!\n");
+        printf("SIGHLD received!\n");
     } else if( sig == SIGPIPE ) {
         signal(SIGPIPE, sig_handler); /* reset it to this function */
-        debug_info("SIGPIPE received!\n");
+        printf("SIGPIPE received!\n");
     } else if( sig == SIGALRM ) {
         signal(SIGALRM, sig_handler); /* reset it to this function */
-        debug_info("SIGALRM received!\n");
+        printf("SIGALRM received!\n");
     } else {
-        debug_info("Signal %d received!\n", sig);
+        printf("Signal %d received!\n", sig);
         exit(0);
     }
 }
@@ -166,14 +174,17 @@ static int main_loop(libpd_cfg_t *cfg)
 {
     int rv;
     wrp_msg_t wrp_msg;
-    libpd_instance_t hpd_instance;
+    libpd_instance_t hpd;
     int backoff_retry_time = 0;
     int max_retry_sleep = (1 << 9) - 1;
     int c = 2;
+    char source[128];
+    char destination[128];
 
     while( true ) {
-        rv = libparodus_init( &hpd_instance, cfg );
+        rv = libparodus_init( &hpd, cfg );
         if( 0 != rv ) {
+            printf("Init for parodus (url %s): %d '%s'\n", cfg->parodus_url, rv, libparodus_strerror(rv) );	
             backoff_retry_time = (1 << c) - 1;
             sleep(backoff_retry_time);
             c++;
@@ -182,39 +193,39 @@ static int main_loop(libpd_cfg_t *cfg)
                 c = 2;
                 backoff_retry_time = 0;
             }
-        }
-        debug_info("Init for parodus (url %s): %d '%s'\n", cfg->parodus_url, rv, libparodus_strerror(rv) );
-        if( 0 == rv ) {
+          libparodus_shutdown(&hpd);
+          continue;
+        }  // else
+            printf("producer service registered with libparodus url %s\n", cfg->parodus_url);
             break;
-        }
-        libparodus_shutdown(&hpd_instance);
     }
 
-    debug_print("starting the main loop...\n");
+    snprintf(source,127,"mac:/%s/%s", mac_address, SERVICE_NAME);
+    snprintf(destination, 127, "node-change:");
+
+    printf("starting the main loop...\n");
     do {
         memset(&wrp_msg, 0, sizeof(wrp_msg_t));
         wrp_msg.msg_type = WRP_MSG_TYPE__EVENT;     
-         /* Todo 
-         Create the rest of the message for node-change
-         */                       
-        wrp_msg.u.event.source = strdup("producer"); // ??
-        wrp_msg.u.event.dest   = strdup("someone");  // ??
-	wrp_msg.u.event.payload = strdup("node-change:mac..."); // ??
-        wrp_msg.u.event.payload_size = strlen(wrp_msg.u.event.payload); 
+
+	wrp_msg.u.event.source = strdup(source);
+        wrp_msg.u.event.dest   = strdup(destination);
+	wrp_msg.u.event.payload = NULL;
+        wrp_msg.u.event.payload_size = 0;
         
         
-        rv = libparodus_send(hpd_instance, &wrp_msg);
+        rv = libparodus_send(hpd, &wrp_msg);
         if (0 == rv) {
-            debug_print("producer sent message with no errors\n");
+            printf("producer sent message with no errors\n");
         } else {
-             debug_print("producer send error %d\n", rv);           
+             printf("producer send error %d\n", rv);
         }
-        sleep(60);
+        sleep(10); // should be 60
        
     } while (true);
 
-    (void ) libparodus_shutdown(&hpd_instance);
-    debug_print("End of producer\n");
+    (void ) libparodus_shutdown(&hpd);
+    printf("End of producer\n");
     return 0;
 }
 
@@ -224,3 +235,6 @@ void _help(char *a, char *b)
     
     // stub
 }
+
+
+
