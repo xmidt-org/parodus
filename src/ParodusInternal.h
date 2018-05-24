45c1865cb105d11ca7d699ac3910b96ef8fe29d3
Sadhyama Vengilat <Sadhyama_Vengilat2@cable.comcast.com>
2018-05-24 11:54:48 +0530

    Added queue mechanism to handle CRUD upsteam & downstream

--- src/ParodusInternal.h
+++ src/ParodusInternal.h
@@ -96,6 +96,9 @@ void parStrncpy(char *destStr, const char *srcStr, size_t destSize);
 
 char* getWebpaConveyHeader();
 
+void *CRUDHandlerTask();
+void addCRUDmsgToQueue(wrp_msg_t *crudMsg);
+
 #ifdef __cplusplus
 }
 #endif
