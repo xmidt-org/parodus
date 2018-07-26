1e05869dedb4c026b1ad3a6448c98b10d5ea2835
Gayathri <gayathri_buddappagari@cable.comcast.com>
2018-07-26 17:27:26 +0530

    To validate array of partner_ids

--- src/partners_check.c
+++ src/partners_check.c
@@ -65,12 +65,29 @@ static void parse_partner_id(char *partnerId, char ***partnersList, int *retCoun
 
 int validate_partner_id(wrp_msg_t *msg, partners_t **partnerIds)
 {
-    int matchFlag = 0, i = 0, count = 0;
+    int matchFlag = 0, i = 0, count = 0, partnerCount = 0, j = 0;
+    char **partnersList = NULL;
+    char *partnerId = NULL;
     ParodusPrint("********* %s ********\n",__FUNCTION__);
-    char *partnerId = get_parodus_cfg()->partner_id;
-    if(strlen(partnerId) <= 0)
+    char *temp = get_parodus_cfg()->partner_id;
+    ParodusPrint("temp = %s\n",temp);
+    if(strlen(temp) > 0)
+    {
+        partnerId = strdup(temp);
+    }
+    ParodusPrint("partnerId = %s\n",partnerId);
+    if(partnerId != NULL)
+    {
+        parse_partner_id(partnerId, &partnersList, &partnerCount);
+    }
+    ParodusPrint("partnerCount = %d\n", partnerCount);
+    if(partnerCount == 0)
     {
         ParodusPrint("partner_id is not available to validate\n");
+        if(partnerId != NULL)
+        {
+            free(partnerId);
+        }
         return 0;
     }
     
@@ -82,36 +99,54 @@ int validate_partner_id(wrp_msg_t *msg, partners_t **partnerIds)
             ParodusPrint("partner_ids count is %d\n",count);
             for(i = 0; i < count; i++)
             {
-                if(strcmp(partnerId, msg->u.event.partner_ids->partner_ids[i]) == 0)
+                for(j = 0; j<partnerCount; j++)
                 {
-                    ParodusInfo("partner_id match found\n");
-                    matchFlag = 1;
+                    ParodusPrint("partnersList[%lu] = %s\n",j, partnersList[j]);
+                    if(strcmp(partnersList[j], msg->u.event.partner_ids->partner_ids[i]) == 0)
+                    {
+                        ParodusInfo("partner_id match found\n");
+                        matchFlag = 1;
+                        break;
+                    }
+                }
+                if(matchFlag == 1)
+                {
                     break;
                 }
             }
             
             if(matchFlag != 1)
             {
-                (*partnerIds) = (partners_t *) malloc(sizeof(partners_t) + (sizeof(char *) * (count+1)));
-                (*partnerIds)->count = count+1;
+                (*partnerIds) = (partners_t *) malloc(sizeof(partners_t) + (sizeof(char *) * (count+partnerCount)));
+                (*partnerIds)->count = count+partnerCount;
                 for(i = 0; i < count; i++)
                 {
                     (*partnerIds)->partner_ids[i] = msg->u.event.partner_ids->partner_ids[i];
                     ParodusPrint("(*partnerIds)->partner_ids[%d] : %s\n",i,(*partnerIds)->partner_ids[i]);
                 }
-                (*partnerIds)->partner_ids[count] = (char *) malloc(sizeof(char) * 64);
-                parStrncpy((*partnerIds)->partner_ids[count], partnerId, 64);
-                ParodusPrint("(*partnerIds)->partner_ids[%d] : %s\n",count,(*partnerIds)->partner_ids[count]);
+                j = 0;
+                for(i = count; i<(count+partnerCount); i++)
+                {
+                    (*partnerIds)->partner_ids[i] = (char *) malloc(sizeof(char) * 64);
+                    parStrncpy((*partnerIds)->partner_ids[i], partnersList[j], 64);
+                    ParodusPrint("(*partnerIds)->partner_ids[%d] : %s\n",i,(*partnerIds)->partner_ids[i]);
+                    j++;
+                }
             }
         }
         else
         {
             ParodusPrint("partner_ids list is NULL\n");
-            (*partnerIds) = (partners_t *) malloc(sizeof(partners_t) + (sizeof(char *) * 1));
-            (*partnerIds)->count = 1;
-            (*partnerIds)->partner_ids[0] = (char *) malloc(sizeof(char) * 64);
-            parStrncpy((*partnerIds)->partner_ids[0], partnerId, 64);
-            ParodusPrint("(*partnerIds)->partner_ids[0] : %s\n",(*partnerIds)->partner_ids[0]);
+            (*partnerIds) = (partners_t *) malloc(sizeof(partners_t) + (sizeof(char *) * partnerCount));
+            (*partnerIds)->count = partnerCount;
+            j=0;
+            for(i = 0; i<partnerCount; i++)
+            {
+                (*partnerIds)->partner_ids[i] = (char *) malloc(sizeof(char) * 64);
+                parStrncpy((*partnerIds)->partner_ids[i], partnersList[j], 64);
+                ParodusPrint("(*partnerIds)->partner_ids[%d] : %s\n",i,(*partnerIds)->partner_ids[i]);
+                j++;
+            }
         }
     }
     else if(msg->msg_type == WRP_MSG_TYPE__REQ)
@@ -122,17 +157,25 @@ int validate_partner_id(wrp_msg_t *msg, partners_t **partnerIds)
             ParodusPrint("partner_ids count is %d\n",count);
             for(i = 0; i < count; i++)
             {
-                if(strcmp(partnerId, msg->u.req.partner_ids->partner_ids[i]) == 0)
+                for(j = 0; j<partnerCount; j++)
                 {
-                    ParodusInfo("partner_id match found\n");
-                    matchFlag = 1;
-                    break;
+                    ParodusPrint("partnersList[%lu] = %s\n",j, partnersList[j]);
+                    if(strcmp(partnersList[j], msg->u.req.partner_ids->partner_ids[i]) == 0)
+                    {
+                        ParodusInfo("partner_id match found\n");
+                        matchFlag = 1;
+                        break;
+                    }
                 }
             }
 
             if(matchFlag != 1)
             {
-                ParodusError("Invalid partner_id %s\n",partnerId);
+                ParodusError("Invalid partner_id %s\n",temp);
+                if(partnerId != NULL)
+                {
+                    free(partnerId);
+                }
                 return -1;
             }
         }
@@ -141,7 +184,10 @@ int validate_partner_id(wrp_msg_t *msg, partners_t **partnerIds)
             ParodusPrint("partner_ids list is NULL\n");
         }
     }
-    
+    if(partnerId != NULL)
+    {
+        free(partnerId);
+    }
     return 1;
 }
 
