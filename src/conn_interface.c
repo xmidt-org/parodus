463970959318ef9927c29a4721ccd6ce75405a9c
Sreedevi V <Sreedevi_V@comcast.com>
2021-09-23 22:08:19 +0530

    Listener for webconfig upstream events when bin distro is on

--- src/conn_interface.c
+++ src/conn_interface.c
@@ -112,7 +112,9 @@ void createSocketConnection(void (* initKeypress)())
     UpStreamMsgQ = NULL;
     StartThread(handle_upstream, &upstream_tid);
     StartThread(processUpstreamMessage, &upstream_msg_tid);
+    #ifdef ENABLE_WEBCFGBIN
     registerRBUSlistener();
+    #endif 
     ParodusMsgQ = NULL;
     StartThread(messageHandlerTask, &downstream_tid);
     StartThread(serviceAliveTask, &svc_alive_tid);
